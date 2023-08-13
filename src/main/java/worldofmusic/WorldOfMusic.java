8137f0ad5cb39aeb04d2d93dfd097e4f9587ebe2
LoserEXE <mr.moomoocower167@gmail.com>
2023-08-13 00:45:31 -0500

    Removed summonPatrol command

--- src/main/java/worldofmusic/WorldOfMusic.java
+++ src/main/java/worldofmusic/WorldOfMusic.java
@@ -2,12 +2,6 @@ package worldofmusic;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.text.Text;
-import net.minecraft.world.gen.feature.ConfiguredStructureFeatures;
-import net.minecraft.world.spawner.PatrolSpawner;
-import net.minecraft.world.spawner.Spawner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import worldofmusic.entity.ModEntities;
@@ -23,21 +17,5 @@ public class WorldOfMusic implements ModInitializer {
         ModItems.registerItems();
         ModEntities.registerEntities();
         ModPackets.registerC2SPackets();
-
-        CommandRegistrationCallback.EVENT.register((dispatcher , unused) -> dispatcher.register(CommandManager.literal("summonPatrol")
-                .executes(context -> {
-                    while(true) {
-                        Spawner spawner = new PatrolSpawner();
-                        int status = spawner.spawn(context.getSource().getWorld(), true, true);
-                        if(status != 0) {
-                            context.getSource().getPlayer().sendMessage(Text.of("Summoned Patrol!"), false);
-                            break;
-                        }
-                    }
-
-                    return 1;
-                })));
-
-
     }
 }
