9e11f0c4a1b3cf29854b9a7be38a89995ace4829
LoserEXE <mr.moomoocower167@gmail.com>
2023-08-13 16:43:12 -0500

    Added Bagpipe Songs

--- src/main/java/worldofmusic/item/BagPipeItem.java
+++ src/main/java/worldofmusic/item/BagPipeItem.java
@@ -7,5 +7,14 @@ public class BagPipeItem extends Instrument{
 
         register("bonnie_dundee", PlayCondition.RAID);
         register("blue_bonnets_over_the_border", PlayCondition.RAID);
+        register("amazing_grace", PlayCondition.NONE);
+        register("ballad_of_glencoe", PlayCondition.NONE);
+        register("balmoral", PlayCondition.NONE);
+        register("battle_of_the_somme", PlayCondition.NONE);
+        register("battle_of_waterloo", PlayCondition.NONE);
+        register("cock_o_the_north", PlayCondition.NONE);
+        register("lilliburlero", PlayCondition.NONE);
+        register("scotland_the_brave", PlayCondition.NONE);
+        register("the_black_bear", PlayCondition.NONE);
     }
 }
