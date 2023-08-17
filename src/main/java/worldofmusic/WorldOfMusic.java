379264732e44ef0eb09ddf9c835a0d3f02ba5d81
LoserEXE <mr.moomoocower167@gmail.com>
2023-08-16 21:09:40 -0500

    Added summonPatrol command back for testing

--- src/main/java/worldofmusic/WorldOfMusic.java
+++ src/main/java/worldofmusic/WorldOfMusic.java
@@ -1,7 +1,14 @@
 package worldofmusic;
 
+import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+import net.minecraft.world.spawner.PatrolSpawner;
+import net.minecraft.world.spawner.Spawner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import worldofmusic.entity.ModEntities;
@@ -17,5 +24,26 @@ public class WorldOfMusic implements ModInitializer {
         ModItems.registerItems();
         ModEntities.registerEntities();
         ModPackets.registerC2SPackets();
+
+        CommandRegistrationCallback.EVENT.register((dispatcher , unused) -> dispatcher.register(CommandManager.literal("summonPatrol")
+                .executes(context -> {
+                    ServerCommandSource source = context.getSource();
+
+                    if(source.getWorld().getServer().getTicks() < 120000) {
+                        source.getPlayer().sendMessage(Text.of("World needs to be at least 120,000 ticks old!"), false);
+                        return 0;
+                    }
+
+                    while(true) {
+                        Spawner spawner = new PatrolSpawner();
+                        int status = spawner.spawn(source.getWorld(), true, true);
+                        if(status != 0) {
+                            context.getSource().getPlayer().sendMessage(Text.of("Summoned Patrol!"), false);
+                            break;
+                        }
+                    }
+
+                    return 1;
+                })));
     }
 }
